6f50008a920abf3db165dee2ce2e66bb23fe97b1
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-13 17:19:32 +0200

    Add more test for capitalize

--- capitalize/capitalize.test.js
+++ capitalize/capitalize.test.js
@@ -7,3 +7,19 @@ test('Happy path (example 1)', () => {
 test('Happy path (example 2)', () => {
     expect(capitalize('bottle')).toMatch(/Bottle/);
 });
+
+test('Test all capitals input', () => {
+    expect(capitalize('PLANE')).toMatch(/PLANE/);
+});
+
+test('Test no input', () => {
+    expect(() => capitalize()).toThrow(TypeError);
+});
+
+test('Test input of wrong type (example 1)', () => {
+    expect(() => capitalize(10)).toThrow(TypeError);
+});
+
+test('Test input of wrong type (example 2)', () => {
+    expect(() => capitalize(['a', 'b', 'c'])).toThrow(TypeError);
+});
