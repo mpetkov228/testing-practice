d866dd04ed2ec7b4358f0363362160a98473099b
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-15 12:42:51 +0200

    Write divide function tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -10,13 +10,26 @@ function subtract(a, b) {
     if (typeof a !== 'number' || typeof b !== 'number') {
         throw new TypeError('Invalid input!');
     }
-    
+
     return a - b;
+}
+
+function divide(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('Invalid input!');
+    }
+
+    if (b === 0) {
+        throw new Error('Error! Cannot divide by zero!');
+    }
+
+    return a / b;
 }
 
 const calculator = {
     add,
     subtract,
+    divide,
 }
 
 module.exports = calculator;
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,4 +1,4 @@
-const { add, subtract } = require('./calculator.js')
+const { add, subtract, divide } = require('./calculator.js')
 
 // Add function tests
 test('Add happy path (example 1)', () => {
@@ -7,6 +7,10 @@ test('Add happy path (example 1)', () => {
 
 test('Add happy path (example 2)', () => {
     expect(add(5, 3)).toBe(8);
+});
+
+test('Add floating point numbers', () => {
+    expect(add(0.2, 0.3)).toBeCloseTo(0.5);
 });
 
 test('Test on no input', () => {
@@ -38,6 +42,10 @@ test('Subtract happy path (example 2)', () => {
     expect(subtract(12, 4)).toBe(8);
 });
 
+test('Subtract floating point numbers', () => {
+    expect(subtract(0.3, 0.2)).toBeCloseTo(0.1);
+});
+
 test('Subtract when a < b', () => {
     expect(subtract(3, 6)).toBe(-3);
 });
@@ -60,4 +68,49 @@ test('Test on arguments of wrong type (example 2)', () => {
 
 test('Test on arguments of wrong type (example 3)', () => {
     expect(() => subtract(2, [1, 2])).toThrow(TypeError);
+});
+
+// Divide function tests
+test('Divide happy path (example 1)', () => {
+    expect(divide(4, 2)).toBe(2);
+});
+
+test('Divide happy path (example 2)', () => {
+    expect(divide(9, 3)).toBe(3);
+});
+
+test('Divide floating point numbers (example 1)', () => {
+    expect(divide(0.4, 0.2)).toBe(2)
+});
+
+test('Divide floating point numbers (example 2)', () => {
+    expect(divide(0.7, 3)).toBeCloseTo(0.23)
+});
+
+test('Divide when a < b', () => {
+    expect(divide(1, 2)).toBeCloseTo(0.5);
+});
+
+test('Division by zero', () => {
+    expect(() => divide(3, 0)).toThrow(/divide by zero/);
+});
+
+test('Test on no input', () => {
+    expect(() => divide()).toThrow(TypeError);
+});
+
+test('Test on wrong number of arguments', () => {
+    expect(() => divide(2)).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 1)', () => {
+    expect(() => divide('ab', 'cd')).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 2)', () => {
+    expect(() => divide(2, 'cd')).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 3)', () => {
+    expect(() => divide([1, 2], 3)).toThrow(TypeError);
 });
