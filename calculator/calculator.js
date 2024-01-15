a32ac1435df9be8ec11cd2b560ec01532d6769b9
MihailPetkov96 <mpetkov228@gmail.com>
2024-01-15 12:22:03 +0200

    Write subtract tests

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,22 @@
+function add(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('Invalid input!');
+    }
+    
+    return a + b;
+}
+
+function subtract(a, b) {
+    if (typeof a !== 'number' || typeof b !== 'number') {
+        throw new TypeError('Invalid input!');
+    }
+    
+    return a - b;
+}
+
 const calculator = {
-    add(a, b) {
-        if (typeof a !== 'number' || typeof b !== 'number') {
-            throw new TypeError('Invalid input!');
-        }
-        
-        return a + b;
-    },
+    add,
+    subtract,
 }
 
 module.exports = calculator;
--- calculator/calculator.test.js
+++ calculator/calculator.test.js
@@ -1,31 +1,63 @@
-const calculator = require('./calculator.js')
+const { add, subtract } = require('./calculator.js')
 
 // Add function tests
 test('Add happy path (example 1)', () => {
-    expect(calculator.add(1, 2)).toBe(3);
+    expect(add(1, 2)).toBe(3);
 });
 
 test('Add happy path (example 2)', () => {
-    expect(calculator.add(5, 3)).toBe(8);
+    expect(add(5, 3)).toBe(8);
 });
 
 test('Test on no input', () => {
-    expect(() => calculator.add()).toThrow(TypeError);
+    expect(() => add()).toThrow(TypeError);
 });
 
 test('Test on wrong number of arguments', () => {
-    expect(() => calculator.add(1)).toThrow(TypeError);
+    expect(() => add(1)).toThrow(TypeError);
 });
 
 test('Test on arguments of wrong type (example 1)', () => {
-    expect(() => calculator.add('ab', 'cd')).toThrow(TypeError);
+    expect(() => add('ab', 'cd')).toThrow(TypeError);
 });
 
 test('Test of arguments of wrong type (example 2)', () => {
-    expect(() => calculator.add('ab', 2)).toThrow(TypeError);
+    expect(() => add('ab', 2)).toThrow(TypeError);
+});
+
+test('Test of arguments of wrong type (example 3)', () => {
+    expect(() => add([1, 2], 2)).toThrow(TypeError);
 });
 
 // Subtract function tests
 test('Subtract happy path (example 1)', () => {
-    expect(calculator.subtract(2, 1)).toBe(1);
+    expect(subtract(2, 1)).toBe(1);
+});
+
+test('Subtract happy path (example 2)', () => {
+    expect(subtract(12, 4)).toBe(8);
+});
+
+test('Subtract when a < b', () => {
+    expect(subtract(3, 6)).toBe(-3);
+});
+
+test('Test on no input', () => {
+    expect(() => subtract()).toThrow(TypeError);
+});
+
+test('Test on wrong number of arguments', () => {
+    expect(() => subtract(4)).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 1)', () => {
+    expect(() => subtract('ab', 'cd')).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 2)', () => {
+    expect(() => subtract(4, 'cd')).toThrow(TypeError);
+});
+
+test('Test on arguments of wrong type (example 3)', () => {
+    expect(() => subtract(2, [1, 2])).toThrow(TypeError);
 });
